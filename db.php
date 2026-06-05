57e4111287f47ae60469753158590fd52d66efca
Jatin170304 <jatinbairwa882@gmail.com>
2026-06-05 15:16:39 +0530

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ $pass = "";
 $dbname = "user_auth";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
-
+// Test GitHub update
 if(!$conn){
     die("Connection Failed: ".mysqli_connect_error());
 }
